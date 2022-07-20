71ed54d6977b432166a366093e3e1fdecc6e6c93
e.iliadis <e.iliadis@pmmretail.com>
2022-07-20 17:09:01 +0300

    Logger configuration and initialized in the main class.

---
+++ src/main/java/Team3/JavaMiniProject/ACMEappMain.java
@@ -0,0 +1,11 @@
+package Team3.JavaMiniProject;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ACMEappMain {
+    private static final Logger logger = LogManager.getLogger(ACMEappMain.class);
+    public static void main(String[] args) {
+
+    }
+
+}
--- src/main/java/Team3JavaMiniProject/ACMEappMain.java
+++
@@ -1,9 +0,0 @@
-package Team3JavaMiniProject;
-
-public class ACMEappMain {
-
-    public static void main(String[] args) {
-
-    }
-
-}
